b752026604bb96539f0c56b2915ca518d87004fa
Crequer, Jimmy | Jim | ECFD <jimmy.crequer@rakuten.com>
2019-09-11 14:38:34 +0900

    Add refetch

--- ui/src/QuestionComponent.js
+++ ui/src/QuestionComponent.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import gql from "graphql-tag";
 import { compose, graphql } from "react-apollo";
+import { ApolloConsumer } from "react-apollo";
+import { Query } from "react-apollo";
+
 
 const QUERY_KANJI = gql`
     query Kanji($level: String!) {
@@ -10,82 +13,28 @@ const QUERY_KANJI = gql`
     }
 `
 
-class QuestionComponent extends Component {
+export default class QuestionComponent extends Component {
     constructor(props) {
         super(props)
-
-        this.state = {
-            aaa: "Hello"
-        }
     }
 
-    async queryKanji() {
-        const { randomKanji } = this.props
-        const result = await randomKanji({ variables: {
-            'level': "N5"
-        }})
-
-        console.log(result)
-    }
-
-    renderQuestion() {
-        // console.log('render')
-        // return (
-        //     <Query 
-        //         query={gql`
-        //             query Kanji($level: String!) {
-        //                 randomKanji(level: $level) {
-        //                     value
-        //                 }
-        //             }
-        //         `}
-        //         variables={{
-        //             level: "N5"
-        //         }}
-        //     >
-        //     {({loading, error, data}) => {
-        //         if (loading) return <p>Loading...</p>
-        //         if (error) return <p>Error</p>
-
-        //         return <h2 onClick={this.handleClick}>QuestionComponent {data.randomKanji.value}</h2>
-        //     }}
-        //     </Query>
-        // )
-    }
-
-    handleClick = () => {
+    handleClick(refetch) {
         console.log("handleClick")
-        //this.props.onClick()
-        //this.renderQuestion()
-        // this.renderQuestion()
-        // this.setState({
-        //     aaa: "World"
-        // })
-
-        // const { loading, error, data } = useLazyQuery(gql`
-        //     query Kanji($level: String!) {
-        //         randomKanji(level: $level) {
-        //             value
-        //         }
-        //     }
-        // `)
-
-        // if (loading) return <p>Loading...</p>;
-        // if (error) return <p>Error :(</p>;
-
-        // console.log(data)
+        refetch()
     }
-
 
     render() {
         return (
-            <div>
-                <h2 onClick={this.queryKanji.bind(this)}>Hello</h2>
-            </div>
+            <Query query={QUERY_KANJI} variables={{level: "N5"}}>
+                {({ loading, error, data, refetch }) => {
+                    if (loading) return "Loading..."
+                    if (error) return `Error! ${error.message}`
+
+                    return (
+                        <p onClick={this.handleClick.bind(this, refetch)}>{data.randomKanji.value}</p>
+                    )
+                }}
+            </Query>
         )
     }
 }
-
-export default compose(
-    graphql(QUERY_KANJI, { name: "randomKanji" })
-)(QuestionComponent)
