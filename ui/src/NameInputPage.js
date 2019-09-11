ddb2f955eb5569d74ab1568e1eb0bce1a52b6e14
taiki <ar-taiki.oguri@rakuten.com>
2019-09-11 17:35:28 +0900

    change ui

--- ui/src/NameInputPage.js
+++ ui/src/NameInputPage.js
@@ -1,85 +1,92 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
 // import {withRouter} from "react-router-dom";
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import gql from "graphql-tag";
-import {compose, graphql} from 'react-apollo'
+import { compose, graphql } from "react-apollo";
 
 const REGISTER_USER = gql`
-mutation User($userName: String!, $userScore: Int) {
-  CreateUser(name: $userName, score: $userScore) {
-    id
-    score
+  mutation User($userName: String!, $userScore: Int) {
+    CreateUser(name: $userName, score: $userScore) {
+      id
+      score
+    }
   }
-}`;
+`;
 
 class NameInputPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userName: "",
-            userId: "",
-            userScore: 0
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      userId: "",
+      userScore: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({userName: event.target.value});
-    }
+  handleChange(event) {
+    this.setState({ userName: event.target.value });
+  }
 
-    async regUser(name) {
-        const {CreateUser} = this.props;
-        const result = await CreateUser({
-            variables: {
-                'userName': name,
-                'userScore': 0
-            }
-        });
-        this.setState({userId: result.data.CreateUser.id});
-        this.setState({userScore: result.data.CreateUser.score});
-        this.handleClick();
-    }
+  async regUser(name) {
+    const { CreateUser } = this.props;
+    const result = await CreateUser({
+      variables: {
+        userName: name,
+        userScore: 0
+      }
+    });
+    this.setState({ userId: result.data.CreateUser.id });
+    this.setState({ userScore: result.data.CreateUser.score });
+    this.handleClick();
+  }
 
-    handleClick() {
-        this.props.history.push({
-            pathname: "/quiz",
-            state: {
-                userName: this.state.userName,
-                userId: this.state.userId,
-                userScore: this.state.userScore,
-            }
-        });
-    }
+  handleClick() {
+    this.props.history.push({
+      pathname: "/quiz",
+      state: {
+        userName: this.state.userName,
+        userId: this.state.userId,
+        userScore: this.state.userScore
+      }
+    });
+  }
 
-    render() {
-        return (
-            <>
-                <h1 className="text-light font-weight-bold align-middle logo-line text-center mb-5">
-                    &#x1F344;
-                    Fun<span className="kanji">字</span>
-                </h1>
-                <Container>
-                    <Row>
-                        <Col md={6} className="text-center">
-                            <Form.Control type="text" placeholder="your name..." value={this.state.userName}
-                                          onChange={this.handleChange} className="mb-2"/>
-                            <Button variant="danger" type="button"
-                                    onClick={this.regUser.bind(this, this.state.userName)}
-                                    onTouch={this.regUser.bind(this, this.state.userName)}
-                                disabled={this.state.userName === ""}
-                            >
-                                Start!
-                            </Button>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <h1 className="text-light font-weight-bold align-middle logo-line text-center mb-5">
+          &#x1F344; Fun<span className="kanji">字</span>
+        </h1>
+        <Container>
+          <Row className="align-items-center justify-content-center">
+            <Col md={6} className="text-center">
+              <Form.Control
+                type="text"
+                placeholder="your name..."
+                value={this.state.userName}
+                onChange={this.handleChange}
+                className="mb-2"
+              />
+              <Button
+                variant="danger"
+                type="button"
+                onClick={this.regUser.bind(this, this.state.userName)}
+                onTouch={this.regUser.bind(this, this.state.userName)}
+                disabled={this.state.userName === ""}
+              >
+                Start!
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
-export default compose(
-    graphql(REGISTER_USER, {name: 'CreateUser'})
-)(NameInputPage);
+export default compose(graphql(REGISTER_USER, { name: "CreateUser" }))(
+  NameInputPage
+);
--- ui/src/QuizPage.js
+++ ui/src/QuizPage.js
@@ -1,79 +1,81 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import QuestionComponent from "./QuestionComponent";
 
 class QuizPage extends Component {
-    constructor(props) {
-        super(props);
-        this.array = [];
-        this.state = {
-            currentLevel: "N5",
-            isAnswered: false,
-            data: null,
-            quize: null,
-            isClicked: false,
-            score: 0,
-            streak: 0
-        }
+  constructor(props) {
+    super(props);
+    this.array = [];
+    this.state = {
+      currentLevel: "N5",
+      isAnswered: false,
+      data: null,
+      quize: null,
+      isClicked: false,
+      score: 0,
+      streak: 0
+    };
+  }
+
+  updateScore = (isCorrect, points) => {
+    var newScore = this.state.score + (isCorrect ? points : 0);
+    var newStreak = this.state.streak + (isCorrect ? 1 : -1);
+    var newLevel = this.state.currentLevel;
+
+    if (newStreak == 2) {
+      newStreak = 0;
+      newLevel = this.changeLevel(newLevel, 1);
+    } else if (newStreak == -2) {
+      newStreak = 0;
+      newLevel = this.changeLevel(newLevel, -1);
     }
 
-    updateScore = (isCorrect, points) => {
-        var newScore = this.state.score + (isCorrect ? points : 0)
-        var newStreak = this.state.streak + (isCorrect ? 1 : -1)
-        var newLevel = this.state.currentLevel
-        
-        if (newStreak == 2) {
-            newStreak = 0
-            newLevel = this.changeLevel(newLevel, 1)
-        }
-        else if (newStreak == -2) {
-            newStreak = 0
-            newLevel = this.changeLevel(newLevel, -1)
-        } 
+    console.log("updateScore", newScore, newStreak, newLevel);
 
-        console.log("updateScore", newScore, newStreak, newLevel)
-        
-        this.setState({
-            score: newScore,
-            streak: newStreak,
-            currentLevel: newLevel
-        })
+    this.setState({
+      score: newScore,
+      streak: newStreak,
+      currentLevel: newLevel
+    });
+  };
+
+  changeLevel(current, change) {
+    const LEVELS = ["N5", "N4", "N3", "N2", "N1"];
+
+    var i = LEVELS.indexOf(current);
+    var newI = i + change;
+
+    if (newI < 0) newI = 0;
+    else if (newI == LEVELS.length) {
+      newI = LEVELS.length - 1;
     }
 
-    changeLevel(current, change) {
-        const LEVELS = ["N5", "N4", "N3", "N2", "N1"]
+    return LEVELS[newI];
+  }
 
-        var i = LEVELS.indexOf(current)
-        var newI = i + change
-
-        if (newI < 0 )
-            newI = 0
-        else if (newI == LEVELS.length) {
-            newI = LEVELS.length - 1
-        }
-
-        return LEVELS[newI]
-    }
-
-    render() {
-        return (
-            <>
-                <h1 className="text-light font-weight-bold align-middle logo-line text-center mb-5">
-                    &#x1F344;
-                    Fun<span className="kanji">字</span>
-                </h1>
-                <Container>
-                    {this.state.score}
-                    <Row className="align-items-center h-100 justify-content-center">
-                        <Col md={6}>
-                        <QuestionComponent updateScore={this.updateScore} level={this.state.currentLevel}></QuestionComponent>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <h1 className="text-light font-weight-bold align-middle logo-line text-center mb-3">
+          &#x1F344; Fun<span className="kanji">字</span>
+        </h1>
+        <Container>
+          <h5 className="text-center mb-5">
+            {this.props.location.state.userName}: {this.state.score} points
+          </h5>
+          <Row className="align-items-center">
+            <Col md={6}>
+              <QuestionComponent
+                updateScore={this.updateScore}
+                level={this.state.currentLevel}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default QuizPage;
