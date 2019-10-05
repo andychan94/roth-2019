df3fbb2b647e4dd191c93368575aa14189f1953f
Nodir <nodir.rashidov@rakuten.com>
2019-10-05 18:07:16 +0900

    add vuejs dir, start making first screen

---
+++ ui-vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-full-mobile is-8-tablet is-offset-2-tablet is-4-desktop is-offset-4-desktop">
+          <b-field>
+            <b-input placeholder="your name..."
+                     type="text"
+                     required
+                     validation-message="Cannot be empty">
+            </b-input>
+          </b-field>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-full-mobile is-8-tablet is-offset-2-tablet is-4-desktop is-offset-4-desktop has-text-centered">
+          <b-button type="is-danger">Next!</b-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      clickMe() {
+        this.$buefy.notification.open('Clicked!!')
+      }
+    }
+  }
+</script>
